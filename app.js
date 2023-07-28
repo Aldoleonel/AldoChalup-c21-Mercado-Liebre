24e69e2eb0d0f60be4a05fdb05f7e59f1b617909
Aldoleonel <aldoleonelchalup@gmail.com>
2023-07-28 00:03:26 -0300

    Registro y Login terminados

--- app.js
+++ app.js
@@ -4,4 +4,6 @@ const path = require('path');
 const PORT = 3030;
 app.use(express.static(path.join(__dirname,'public',)));
 app.get('/',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'home.html')));
+app.get('/login',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'login.html')));
+app.get('/registro',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'registro.html')));
 app.listen (PORT,()=>console.log("servidor funcionando en puerto 3030"));
